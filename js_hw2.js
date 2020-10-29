84822ea4a868e128df392e45a63c9b013b768ee3
OksanaVinichuk <OViniTheChuk@gmail.com>
2020-10-29 15:06:24 +0200

    js_hw2

---
+++ js_hw2.js
@@ -0,0 +1,9 @@
+// 1. --створити масив та вивести його в консоль:
+//    - з 5 числових значень
+//    - з 5 стічкових значень
+//    - з 5 значень стрічкового, числового та булевого типу
+let numArray
+[4, 27, 93, 34, 8];
+console.log(numArray);
+let strArray["Hello","Okten","Web","University"];
+console.log(strArray);
